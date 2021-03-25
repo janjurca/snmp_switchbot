c14e5567883e9d0565c9d1a582fea940be58cb67
Jan Jurca <jjurca@redhat.com>
2021-03-25 14:33:04 +0100

    ip, mask, gw

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,9 @@ Scheduler sched = Scheduler();
 static byte mac[] = { 0xE2, 0x21, 0xC9, 0x36, 0xCA, 0xF6 };
 static byte ip[] = { 10, 43, 134, 175 };
 
-static byte gateway[] = { 10, 43, 134, 1 };  // NOT used if line ETHERNET BEGIN is commented (41)
-static byte dns[] = { 1, 1, 1, 1 };  // NOT used if line ETHERNET BEGIN is commented (41)
-static byte subnet[] = { 255, 255, 255, 0 };  // NOT used if line ETHERNET BEGIN is commented (41)
+static byte gateway[] = { 10, 43, 135, 254 };  // NOT used if line ETHERNET BEGIN is commented (41)
+static byte dns[] = {10, 38, 5, 26 };  // NOT used if line ETHERNET BEGIN is commented (41)
+static byte subnet[] = { 255, 255, 254, 0 };  // NOT used if line ETHERNET BEGIN is commented (41)
 
 SNMP_API_STAT_CODES api_status;
 
@@ -37,8 +37,8 @@ void setup()
   Serial.begin(9600);
   sw.begin();
 
-  Ethernet.begin(mac, ip);
-  //Ethernet.begin(mac, ip, dns, gateway, subnet);
+  //Ethernet.begin(mac, ip);
+  Ethernet.begin(mac, ip, dns, gateway, subnet);
   api_status = Agentuino.begin();
   if ( api_status == SNMP_API_STAT_SUCCESS ) {
     Agentuino.onPduReceive(proccess);
